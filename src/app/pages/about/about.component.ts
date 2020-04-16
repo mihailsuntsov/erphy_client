56f1a6cb9a191838ea64cc8fcab3902d92b9bc49
Mihail Suntsov <mihail.suntsov@yandex.ru>
2020-04-17 02:44:51 +0500

    Перед переделкой на обращение к БД

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  content = '<h1>\n  Загруженный контент HTML-страницы "О нас", содержащей встроенные компоненты.\n</h1>\n<h2>А это сам встроенный компонент:</h2>\n<example-component name="hello">Контент, переданный в example-компонент.</example-component>\n<h2>А это второй встроенный компонент:</h2>\n<example2-component name="hello">Контент, переданный в example2-компонент.</example2-component>';
+  content:string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loadContent();
   }
+
+  loadContent(){
+    this.content = '<h1>\n  Загруженный контент HTML-страницы "О НАС", содержащей встроенные компоненты.\n</h1>\n<h2>А это сам встроенный компонент:</h2>\n<example-component name="hello">Контент, переданный в example-компонент.</example-component>\n<h2>А это второй встроенный компонент:</h2>\n<example2-component name="hello">Контент, переданный в example2-компонент.</example2-component>';
+  }
+
 
 }
