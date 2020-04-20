b3f85aefcb0576a9423dfb188e1e5e6dad5f17c1
Mihail Suntsov <mihail.suntsov@yandex.ru>
2020-04-20 16:33:13 +0500

    Компонент вставлен, работает и с параметром, и с ng-template

--- src/app/embeddable-components/example.component.ts
+++ src/app/embeddable-components/example.component.ts
@@ -3,12 +3,18 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'example-component',
   template: `
-[Начало example-компонента<br><ng-content></ng-content><br>Конец example-компонента<br><example2-component name="hello2">Контент, переданный в example2-компонент из example-компонента</example2-component>]
+[Начало example-компонента<br><ng-content></ng-content><br> 
+
+  <content-viewer [content]="insidecontent" ></content-viewer><br />
+  Конец example-компонента
+]
   `,
 })
 export class ExampleComponent {
   name: string;
+  insidecontent: string='<example2-component name="hello333">Контент, переданный в example2-компонент из example-компонента.</example2-component>';
   ngOnInit(){
     console.log(`ExampleComponent.OnInit name=`, this.name)
+
   }
 }
