49264ee80b8e36f9f9f9f459bcb9f23495719a77
mihailsuntsov <mihail.suntsov@yandex.ru>
2021-02-18 03:32:13 +0500

    Апдейт на Angular 11 завершен - исправлены все ошибки

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,5 +1,5 @@
 import { ModuleWithProviders, NgModule} from "@angular/core";
-import { MAT_LABEL_GLOBAL_OPTIONS, MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatNativeDateModule, } from '@angular/material/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatBadgeModule } from '@angular/material/badge';
